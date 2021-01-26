5340b3660f3851a496889069ad41920daa18cb13
Colin Schlecht <schlechtcolin@yahoo.com>
2021-01-25 21:17:29 -0800

    commit tree d78e5898266aab1f382274b1da78a4fad9fcf725

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,33 @@
+require 'pry'
 class Pokemon
+    attr_accessor :name, :type, :db, :id
+    def initialize(db)
+        @id = db[:id]
+        @name = db[:name]
+        @type = db[:type]
+        @db = db
+    end
+    def self.update(name, type, db)
+        sql = <<-SQL
+        UPDATE pokemon SET name = ?, type = ?, db = ? WHERE ID = ?
+        SQL
+        db.exectue(sql, name, type, db)
+    end
+    def self.save(name, type, db)
+            sql =<<-SQL
+            INSERT INTO pokemon (name, type)
+            values (?, ?)
+            SQL
+            db.execute(sql, name, type)
+            @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
+    def self.find(id, db)
+        sql = <<-SQL
+        SELECT * FROM pokemon
+        WHERE id = ?
+        SQL
+        poke = db.execute(sql, id).flatten
+        new_pokemon = Pokemon.new(id: poke[0], name: poke[1], type: poke[2])
+        
+    end
 end
